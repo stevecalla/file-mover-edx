62d690de0deed74cd1cbc9fbcb3f20035d6e7eed
Steve Calla <callasteven@gmail.com>
2022-08-21 23:51:47 -0600

    Unknown changes

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,6 @@
-const fs = require("fs").promises;
 const { getBasicInfo, getRole, getRoleQuestion, confirmContinue } = require("./runInquirer");
-const { createMembers, createClass } = require("./createMembers.js");
-const { employeeProfileTemplate } = require("../../src/employeeProfileTemplate.html");
-const template = require("../../src/homePageTemplate.html");
-const Employee = require('../../lib/employee.js');
-const Intern = require('../../lib/intern.js');
+const { createMembers } = require("./createMembers.js");
+const { createHTML } = require("./createHTML.js");
 let teamMembers = [];
 
 getTeamDetails = async (role = 'Manager') => {
@@ -28,30 +24,14 @@ inputMoreMembers = async (confirm, role) => {
     role = await getRole(),
     getTeamDetails(role.role)
   ) : (
-    await createMemberHTML()
+    await createHTML(teamMembers)
     // await writeTeamMembers(),
   )
   // console.log(teamMembers);
 }
 
-createMemberHTML = async () => {
-  let membersHTML = "";
-  
-  //USED FOR OF STATEMENT BECAUSE IT SEMED TO WORK LIKE AN AWAIT STATEMENT WHILE MAP & FOR EACH DID NOT EXECUTE BEFORE THE NEXT WRITE FILE STATEMENT
-  for (member of teamMembers) { 
-    console.log(member)
-    membersHTML += employeeProfileTemplate(member);
-  };
-  
-  fs.writeFile("./index-draft.html", template.homePageTemplate(membersHTML), function (err) {
-    if (err) throw err;
-    // console.log('It\'s saved!');
-  });
-  return membersHTML;
-}
-
 module.exports = {
-  getTeamDetails,
+  getTeamDetails
 }
 
 // WRITE INFO TO JSON FILE
