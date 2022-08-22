1c47e0b057f641aa334f9838f56891fc4eb5b192
Steve Calla <callasteven@gmail.com>
2022-08-21 23:45:24 -0600

    Refactor html creation code to include using class getter methods

--- dist/js/createMembers.js
+++ dist/js/createMembers.js
@@ -3,41 +3,26 @@ const Engineer = require('../../lib/engineer.js');
 const Intern = require('../../lib/intern.js');
 
 createMembers = (role, member, employeeDetails, teamMembers) => {
-  member.role = role;
-  member[Object.keys(employeeDetails)] = Object.values(employeeDetails).join('');
-  
-  // console.log('class = ', member);
-
   switch(role) {
     case ("Engineer"): 
-      const engineer = new Engineer(member.firstName, member.lastName, member.employeeId, member.emailAddress, member.gitHubUserName);
+      const engineer = new Engineer(member.firstName, member.lastName, member.employeeId, member.emailAddress, employeeDetails.gitHubUserName);
       engineer.getRole();
-      engineer.getName();
-      engineer.getIcon();
-      engineer.getShowHide();
       teamMembers.push(engineer); //push employees into an array
       break;
     case ("Intern"): 
-      const intern = new Intern(member.firstName, member.lastName, member.employeeId, member.emailAddress, member.internSchool);
+      const intern = new Intern(member.firstName, member.lastName, member.employeeId, member.emailAddress, employeeDetails.internSchool);
       intern.getRole();
-      intern.getName();
-      intern.getIcon();
-      intern.getShowHide();
       teamMembers.push(intern); //push employees into an array
       break;
     default:
-      const manager = new Manager(member.firstName, member.lastName, member.employeeId, member.emailAddress, member.officeNumber);
+      const manager = new Manager(member.firstName, member.lastName, member.employeeId, member.emailAddress, employeeDetails.officeNumber);
       manager.getRole();
-      manager.getName();
-      manager.getIcon();
-      manager.getShowHide();
       teamMembers.push(manager); //push employees into an array
   }
-  console.log(teamMembers);
-  // teamMembers.forEach(element => console.log(element.email));
+  // console.log(teamMembers);
   return teamMembers;
 }
 
 module.exports = {
-  createMembers,
+  createMembers
 }
